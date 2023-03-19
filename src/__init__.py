9abf3d70c35be80ed16a8ffab8540bbb8214f9a0
omshivasai <shivasaigurrapu@gmail.com>
2023-03-19 19:10:34 +0530

    setup_file

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+"""from setup.py where it finds the find_packages() function 
+will check in which folders does the __init__ file is present and it consider whole folder as a package and try to build there.
+And then we can import in other files in the same folder"""
